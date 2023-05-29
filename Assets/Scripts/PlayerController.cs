9c6a755595b790e8784192fbd394969473905a5b
Pakhawat <MR.RYOSHADOW12@gmail.com>
2023-05-29 19:58:07 +0700

    codeupdate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 0.1f;
-    [SerializeField]
-    float boostSpeed = 30f;
-    [SerializeField]
+    bool isTurnleft;
+    bool isTurnright;
+     float turn;
+    [SerializeField] float torqueAmount = 10f;
     float baseSpeed = 20f;
 
     Rigidbody2D rb2d;
     SurfaceEffector2D surfaceEffector2D;
-     bool canMove = true;
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
-         rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+
     }
 
     public void DisableContoller()
@@ -27,37 +30,48 @@ public class PlayerController : MonoBehaviour
         canMove = false;
     }
 
+ private void MovePlayer()
+    {
+        if(isTurnleft) 
+        {
+            turn = torqueAmount;
+        } 
+        else if(isTurnright)
+        {
+            turn = -torqueAmount;
+        } 
+        else
+        {
+            turn = 0;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if (canMove)
-        {
-            RotatePlayer();
-            RespondToBoost();
-        }
+       MovePlayer();
+    }
+    void FixedUpdate()
+    {
+        rb2d.AddTorque(turn);
     }
 
-    void RespondToBoost()
+    public void RotateLeft()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            surfaceEffector2D.speed = boostSpeed;
-        }
-        else
-        {
-            surfaceEffector2D.speed = baseSpeed;
-        }
+        isTurnleft = true;
+
+    }
+    public void RotateRight()
+    {
+        isTurnright = true;
+
+    }
+    public void RotateLeftUp()
+    {
+        isTurnleft = false;
+    }
+    public void RotateRightUp()
+    {
+        isTurnright = false;
     }
 
-    void RotatePlayer()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb2d.AddTorque(torqueAmount);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb2d.AddTorque(-torqueAmount);
-        }
-    }
 }
